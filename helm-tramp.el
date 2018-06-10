07d2f02166038c14052009f6fb9c2a163118e2cc
Masashi Miyaura <masasam@users.noreply.github.com>
2018-06-10 23:17:54 +0900

    Bump version to 1.0.5

--- helm-tramp.el
+++ helm-tramp.el
@@ -4,7 +4,7 @@
 
 ;; Author: Masashı Mıyaura
 ;; URL: https://github.com/masasam/emacs-helm-tramp
-;; Version: 0.9.5
+;; Version: 1.0.5
 ;; Package-Requires: ((emacs "24.3") (helm "2.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
