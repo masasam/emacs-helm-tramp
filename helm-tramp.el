a7456cee86a83d68c966475fab90ab94006fd23c
Masashi Miyaura <masasam@users.noreply.github.com>
2019-01-25 13:47:15 +0900

    Add support for top-level sshconfig include

--- helm-tramp.el
+++ helm-tramp.el
@@ -1,10 +1,10 @@
 ;;; helm-tramp.el --- Tramp helm interface for ssh, docker, vagrant -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2017-2018 by Masashı Mıyaura
+;; Copyright (C) 2017-2019 by Masashı Mıyaura
 
 ;; Author: Masashı Mıyaura
 ;; URL: https://github.com/masasam/emacs-helm-tramp
-;; Version: 1.1.6
+;; Version: 1.2.6
 ;; Package-Requires: ((emacs "24.3") (helm "2.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -75,7 +75,7 @@ Kill all remote buffers."
   (tramp-cleanup-all-buffers))
 
 (defun helm-tramp--candidates (&optional file)
-  "Collect candidates for helm-tramp."
+  "Collect candidates for helm-tramp from FILE."
   (let ((source (split-string
                  (with-temp-buffer
                    (insert-file-contents (or file "~/.ssh/config"))
@@ -106,7 +106,7 @@ Kill all remote buffers."
 	    (push
 	     (concat "/ssh:" host "|sudo:" host ":/")
 	     hosts))))
-      (when (string-match "^Include +\\(.+\\)$" host)
+      (when (string-match "Include +\\(.+\\)$" host)
         (setq include-file (match-string 1 host))
         (when (not (file-name-absolute-p include-file))
           (setq include-file (concat (file-name-as-directory "~/.ssh") include-file)))
